5a8ef4fc1b795203b1a15624b8656cd81ee0ded0
PulsaDenura <87503906+PulsaDenura@users.noreply.github.com>
2025-07-25 21:05:27 +0300

    Delete components/ui directory

--- components/ui/card.js
+++
@@ -1,7 +0,0 @@
-
-export function Card({ children, className }) {
-  return <div className={`bg-white rounded-lg p-4 border ${className}`}>{children}</div>;
-}
-export function CardContent({ children, className }) {
-  return <div className={className}>{children}</div>;
-}
